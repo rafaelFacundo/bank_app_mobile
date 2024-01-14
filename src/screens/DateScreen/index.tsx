932d0561cad84ba7f0923627a758e5bc197b0246
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-01-14 13:56:01 -0300

    age verification added to date screen

--- src/screens/DateScreen/index.tsx
+++ src/screens/DateScreen/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import dayjs from "dayjs";
 import Container from "../../components/Container";
 import ArrowBackButton from "../../components/ArrowBack";
 import { Alert, Platform, View } from "react-native";
@@ -17,6 +18,7 @@ const DateScreen: React.FC<Props> = ({ navigation, route }) => {
   const params = route.params;
   const [showPicker, setShowPicker] = useState(false);
   const [dateToShow, setDateToShow] = useState(new Date(Date.now()));
+  const dateOfToday = new Date();
 
   //functions releated to dateTimeInput
   const onChangeDate = (event: any, value: any) => {
@@ -24,6 +26,16 @@ const DateScreen: React.FC<Props> = ({ navigation, route }) => {
       setShowPicker(false);
     }
     setDateToShow(value);
+  };
+
+  const isTheUserOlderThanEighteen = (
+    todayDate: string,
+    userBirthDate: string
+  ): boolean => {
+    const today = dayjs(todayDate);
+    const birth = dayjs(userBirthDate);
+    const years = birth.diff(today, "year");
+    return years >= 18;
   };
 
   return (
@@ -50,13 +62,28 @@ const DateScreen: React.FC<Props> = ({ navigation, route }) => {
         <MuButton
           text={"Avançar"}
           onPress={() => {
-            if (dateToShow) {
+            const todayDateInSting = `${dateToShow.getFullYear()}-${
+              dateToShow.getMonth() + 1
+            }-${dateToShow.getDate()}`;
+            const userBirthDate = `${dateOfToday.getFullYear()}-${
+              dateOfToday.getMonth() + 1
+            }-${dateOfToday.getDate()}`;
+            const isThisAgeAllowed = isTheUserOlderThanEighteen(
+              todayDateInSting,
+              userBirthDate
+            );
+            //----
+            if (dateToShow && isThisAgeAllowed) {
               navigation.navigate("CreateAccountStack", {
                 screen: "AddressScreenCreate",
                 params: { ...params, birth_date: dateToShow.toDateString() },
               });
+            } else if (dateToShow && !isThisAgeAllowed) {
+              Alert.alert(
+                "É necessário ser maior de idade para fazer o cadastro"
+              );
             } else {
-              Alert.alert("O preenchimento de todos os camos é obrgatório");
+              Alert.alert("O preenchimento de todos os campos é obrigatório");
             }
           }}
         />
